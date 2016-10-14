022f455bad0ccb60848842f449b5beb9ed1fdc12
lucasocon <lucas_masterclas@hotmail.com>
2016-10-14 02:31:01 -0300

    Add new migration to store username Fix seed with new attributes and bit refactor

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160508030834) do
+ActiveRecord::Schema.define(version: 20161014050339) do
 
   create_table "places", force: :cascade do |t|
     t.string   "name"
@@ -55,6 +55,7 @@ ActiveRecord::Schema.define(version: 20160508030834) do
     t.datetime "updated_at",                          null: false
     t.string   "provider"
     t.string   "uid"
+    t.string   "username"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
